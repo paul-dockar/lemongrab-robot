5d4de1e50ee412b6b95180458077ab17a94d607a
Paul Dockar <pdockar@gmail.com>
2016-04-11 12:51:28 +1000

    test

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: Paul Dockar
+ *
+ * Created on 11 April 2016, 12:43 PM
+ */
+
+#include "main.h"
+
+void main (void) {
+    while (1) {
+        continue;
+    }
+}
+
---
+++ main.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.h
+ * Author: Paul Dockar
+ *
+ * Created on 11 April 2016, 12:43 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#include <xc.h>
+#pragma config BOREN = OFF, CPD = OFF, WRT = OFF, FOSC = HS, WDTE = OFF, CP = OFF, LVP = OFF, PWRTE = OFF 
+
+void main (void);
+
+#endif	/* MAIN_H */
+
