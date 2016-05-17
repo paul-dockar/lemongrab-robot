5bf3cf114e06c7b36c92e4d32fe8c0d6013aa4b5
Paul Dockar <pdockar@gmail.com>
2016-05-17 19:29:17 +1000

    current progress: 1)  using another IDE to compile and shit. testing in command line with printf all over the fuckin joint. 2)  will generate neighboring node address'. next step to cycle through the neighbours and generate a 'f' value

--- src/explore.c
+++ src/explore.c
@@ -1,7 +1,12 @@
 #include "explore.h"
 
+unsigned char *closed_set [16];
+unsigned char *open_set [4];
+
 //declare local functions
-void initialisePointersNULL(char *array[], char size);
+void initialisePointersNULL(unsigned char *array[], char size);
+void removeFromOpenSet(unsigned char *item_to_remove);
+unsigned char *getNeighbourNodes(unsigned char *current_node, unsigned char neighbour_node);
 
 void setupExplore(void) {
     setupGlobalMap();
@@ -9,62 +14,70 @@ void setupExplore(void) {
 }
 
 void setupGlobalMap(void) {
-    char x = 0;
-    char y = 0;
-    for (x; x!=4; x++) {
-        for (y; y!=5; y++) {
+    for (char x = 0; x != GLOBAL_X; x++) {
+        for (char y = 0; y != GLOBAL_Y; y++) {
             global_map [x][y] = 0;
         }
     }
 }
-
+    
 void setupLocalMap(void) {
-    char x = 0;
-    char y = 0;
-    for (x; x!=3; x++) {
-        for (y; y!=3; y++) {
+    for (char x = 0; x != LOCAL_X; x++) {
+        for (char y = 0; y != LOCAL_Y; y++) {
             local_map [x][y] = 0;
         }
     }
 }
-
-void writeGlobalMap(char value, char x, char y) {
+    
+void writeGlobalMap(unsigned char value, char x, char y) {
     global_map [x][y] = value;
 }
 
-void writeLocalMap(int adc_distance, char x, char y) {
+void writeLocalMap(unsigned int adc_distance, char x, char y) {
     local_map [x][y] = adc_distance;
 }
 
 char findPathAStar(char robot_x, char robot_y, char goal_x, char goal_y) {
-    char *closed_set [16];
-    char *open_set [4];
-    char *current_open_set;
+    unsigned char *current_open_set = 0;
     
     char g = 0;
     char h = 0;
     char f = g + h;
     
-    initialisePointersNULL(*closed_set, 16);
-    initialisePointersNULL(*open_set, 4);
+    //setup robot/goal position, clear open and closed sets
+    initialisePointersNULL(closed_set, 16);
+    initialisePointersNULL(open_set, 4);
 
-    writeGlobalMap(ROBOT, robot_x, robot_y);
-    writeGlobalMap(GOAL, goal_x, goal_y);
-    
-    global_map[robot_x][robot_y] = f;
-    open_set[0] = &global_map[robot_x][robot_y];
-    
-    while (*open_set [0] != 0) {
-        char smallest_open_set = 255;
+    //writeGlobalMap(ROBOT, robot_x, robot_y);
+    //writeGlobalMap(GOAL, goal_x, goal_y);
+
+    writeGlobalMap(f, robot_x, robot_y);                //put starting node (robot) to 0.
+    open_set[0] = &global_map[robot_x][robot_y];        //put starting node on open set list
+
+    while (*open_set != 0) {
+        unsigned char *successor_up;
+        unsigned char *successor_right;
+        unsigned char *successor_down;
+        unsigned char *successor_left;
+
+        unsigned char smallest_open_set = MAX;
+
+        //find node with least f on the open list
         for (char i = 3; i >= 0; i--) {
-            if (*open_set [i] != 0 && smallest_open_set > *open_set [i]) {
+            if (open_set [i] != 0 && smallest_open_set > *open_set [i]) {
                 smallest_open_set = *open_set [i];
                 current_open_set = open_set [i];
             }
         }
         
+        //remove the chosen one from the open list
+        removeFromOpenSet(current_open_set);
         
-        
+        //generate 4 successors from current node
+        successor_up    = getNeighbourNodes(current_open_set, UP);
+        successor_right = getNeighbourNodes(current_open_set, RIGHT);
+        successor_down  = getNeighbourNodes(current_open_set, DOWN);
+        successor_left  = getNeighbourNodes(current_open_set, LEFT);
     }
     
     
@@ -72,8 +85,37 @@ char findPathAStar(char robot_x, char robot_y, char goal_x, char goal_y) {
     return 0;
 }
 
-void initialisePointersNULL(char *array[], char size) {
+void initialisePointersNULL(unsigned char *array[], char size) {
     for (char i = 0; i < size; i++) {
-        *array[i] = 0;
+        array[i] = 0;
     }
+}
+
+void removeFromOpenSet(unsigned char *item_to_remove) {
+    for (char i = 3; i >= 0; i--) {
+        if (open_set [i] == item_to_remove) {
+            open_set [i] = 0;
+        }
+    }
+}
+
+unsigned char *getNeighbourNodes(unsigned char *current_node, unsigned char neighbour_direction) {
+    for (char x = 0; x != GLOBAL_X; x++) {
+        for (char y = 0; y != GLOBAL_Y; y++) {
+            if (current_node == &global_map[x][y]) {
+                if (x == 0 && neighbour_direction == UP)    return 0;
+                if (x == 4 && neighbour_direction == DOWN)  return 0;
+                if (y == 0 && neighbour_direction == LEFT)  return 0;
+                if (y == 5 && neighbour_direction == RIGHT) return 0;
+
+                switch (neighbour_direction) {
+                    case 1: x--; return (&global_map[x][y]);
+                    case 2: y++; return (&global_map[x][y]);
+                    case 3: x++; return (&global_map[x][y]);
+                    case 4: y--; return (&global_map[x][y]);
+                }
+            }
+        }
+    }
+
 }
--- src/explore.h
+++ src/explore.h
@@ -5,15 +5,24 @@
 
 #define     ROBOT       100
 #define     GOAL        200
-#define     WALL        255
+#define     MAX         255
 #define     DEADEND     254
 #define     CLIFF       253
 
+#define     GLOBAL_X    4
+#define     GLOBAL_Y    5
+#define     LOCAL_X     3
+#define     LOCAL_Y     3
+
+#define     UP          1
+#define     RIGHT       2
+#define     DOWN        3
+#define     LEFT        4
 void setupExplore(void);
 void setupGlobalMap(void);
 void setupLocalMap(void);
-void writeGlobalMap(char, char, char);
-void writeLocalMap(int, char, char);
+void writeGlobalMap(unsigned char, char, char);
+void writeLocalMap(unsigned int, char, char);
 char findPathAStar(char, char, char, char);
 
 bit exploring;
